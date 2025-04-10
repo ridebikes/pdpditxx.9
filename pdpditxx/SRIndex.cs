299bf71e5e13d227421640b8b6507b07e0f36bde
ridebikes <96827443+ridebikes@users.noreply.github.com>
2025-04-10 12:52:44 -0400

    Create SRIndex.cs
    This is the index I created for the new scale and shift (04/2025)

---
+++ pdpditxx/SRIndex.cs
@@ -0,0 +1,16 @@
+
+namespace pdpditxx
+{
+    internal class SRIndex
+    {
+        public int Counter { get; set; }
+        public int Page { get; set; }
+        public int DegreesRotation { set; get; }
+        public int PageSizeX { get; set; }
+        public int PageSizeY { get; set; }
+        public double ScaleX { get; set; }
+        public double ScaleY { get; set; }
+        public double ShiftX { set; get; }
+        public double ShiftY { set; get; }
+    }
+}

8121b293536b46dbda760f4e171ee0958e9cc95c
ridebikes <96827443+ridebikes@users.noreply.github.com>
2025-10-23 11:08:32 -0400

    Annotation Index
    This class is for the annotation index, to add title and contents to specific pages.

---
+++ pdpditxx/AnnotIndex.cs
@@ -0,0 +1,11 @@
+
+namespace pdpditxx
+{
+    internal class AnnotIndex
+    {
+        public int Counter { get; set; }
+        public int Page { get; set; }
+        public string Title { get; set; }
+        public string Contents { get; set; }
+    }
+}
